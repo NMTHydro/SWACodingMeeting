3f3729afadc1deb6f7bc9ea4503fade4e6ade094
jross <jirhiker@gmail.com>
2016-09-12 00:02:37 -0600

    added meeting2

---
+++ meeting_notes/meeting2.md
@@ -0,0 +1,41 @@
+9/13
+
+Current status
+------------------------
+- Slack signup/usage
+- Github signup/usage
+
+- learnpython.org
+- try.git
+
+- one new thing you learned
+
+- anaconda installed?
+- project interpreter set in pycharm 
+
+
+Last Weeks Code Tasks
+----------------------
+- analyze examples
+- discuss good and bad styles/snippets
+- highlight pep8
+
+- intersperse hot key explanations
+- show full suite of hot key examples
+
+
+Todo
+----
+Advanced -
+   write a simple class to describe a car. write a set of classes to describe vehicles, car, boat, plane, etc.
+   **demonstrate your understanding of inheritance**
+   
+Advanced/Beginner - 
+   write a `hello world` script and run it from pycharm - **demonstrate you can run a python script**
+   write a function that tells are car what to do as it approaches a traffic light
+     the states are: 1) green == go 2) red == stop and 3) yellow ==? Think about what your definition of yellow would be
+     and then try to define using code, i.e if/else statements
+     **demonstrate understanding of if/else statements, boolean logic** 
+     
+     
+   
--- src/meeting1_bad.py
+++ src/meeting1_bad.py
@@ -1,22 +1,3 @@
-# ===============================================================================
-# Copyright 2016 ross
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ===============================================================================
-
-# ============= standard library imports ========================
-# ============= local library imports  ==========================
-
 # task1
 p1 = '/Users/ross/Desktop/p1.txt'
 p2 = '/Users/ross/Desktop/p2.txt'
@@ -41,6 +22,3 @@ for li in p1file.readlines():
     else:
         items = li.split(',')
         p2file.write(items[0]+','+items[1] + ',' + items[2])
-
-
-# ============= EOF =============================================
