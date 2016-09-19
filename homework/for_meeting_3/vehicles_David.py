4b3f85ca445ede6f13676c8ead97514b19bf01f8
David Ketchum <dgketchum@gmail.com>
2016-09-19 14:00:03 -0600

    homework, need a test to make sure Boat.power == 'sail' or 'propeller'

---
+++ homework/for_meeting_3/vehicles_David.py
@@ -0,0 +1,27 @@
+class Vehicle(object):
+
+    def __init__(self, make, model, year, kind):
+
+        self.make = make
+        self.model = model
+        self.year = year
+        self.kind = kind
+
+
+class Car(Vehicle):
+
+    def __init__(self, doors):
+
+        Vehicle.__init__(self, self.make, self.model, self.year, kind='Car')
+        self.doors = doors
+
+
+class Boat(Vehicle):
+
+    def __init__(self, power):
+
+        Vehicle.__init__(self, self.make, self.model, self.year, kind='Boat')
+        self.power = power
+
+
+# ============================ EOF =============================
