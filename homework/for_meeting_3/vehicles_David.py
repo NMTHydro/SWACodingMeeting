01fc6f8c399a5a0521827fbfee24431ea1a87fa8
David Ketchum <dgketchum@gmail.com>
2016-09-19 15:46:05 -0600

    added args to test methods

--- homework/for_meeting_3/vehicles_David.py
+++ homework/for_meeting_3/vehicles_David.py
@@ -1,28 +1,38 @@
 class Vehicle(object):
 
-    def __init__(self, make, model, year, kind):
+    def __init__(self, make, model, year):
 
         self.make = make
         self.model = model
         self.year = year
-        self.kind = kind
+
+    def foo(self, a, b, c):
+        print 'does some work'
+        print a, b, c
 
 
 class Car(Vehicle):
 
-    def __init__(self, doors):
+    def __init__(self, doors, *args):
 
-        Vehicle.__init__(self, self.make, self.model, self.year, kind='Car')
+        Vehicle.__init__(self, *args)
         self.doors = doors
+
+    def foo(self, *args, **kw):
+        print args, kw
+        super(Car, self).foo(*args)
 
 
 class Boat(Vehicle):
 
-    def __init__(self, power):
+    def __init__(self, power, *args):
 
-        Vehicle.__init__(self, self.make, self.model, self.year, kind='Boat')
+        Vehicle.__init__(self, *args)
+        if power not in ('propeller', 'sail'):
+            print 'warning: drive type not acceptable'
+            raise TypeError
+
         self.power = power
-        ## ## ##
 
 
 class Airplane(Vehicle):
@@ -30,5 +40,8 @@ class Airplane(Vehicle):
     def __init__(self):
         pass
 
+if __name__ == '__main__':
+    car = Car('honda', 'civic', '2002', '2')
+    car.foo(1,2,3)
 
 # ============================ EOF =============================
